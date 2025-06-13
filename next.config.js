87c3e724256c95649edb66922a0b478149839fb1
Adista Anugrah <adista.anugrah@gmail.com>
2025-06-13 18:51:14 +0700

    fix: Hapus sisa-sisa embedded Sanity Studio

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Tambahkan blok 'images' ini
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-          port: '',
-          pathname: '/images/**',
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  // Konfigurasi ini WAJIB agar komponen <Image> dari Next.js
+  // bisa menampilkan gambar yang di-hosting di Sanity.
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
